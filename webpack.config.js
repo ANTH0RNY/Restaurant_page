f5d9afe57b931edcc6b9a6d087f36c55c2e68c1d
ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>
2023-08-12 09:27:57 +0300

    Done with setting up the project structure

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    mode: "development",
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader','sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'Restaurant page',
+            body: 'hello world'
+        })
+    ]
+}
