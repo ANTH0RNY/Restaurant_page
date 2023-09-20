ac804dd1b7b525fa98634d21885da96f4b1b3022
ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>
2023-09-20 11:02:38 +0300

    Done with basic functionality of image slider

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -5,14 +5,16 @@ import img3 from "../img/scrollbar/balls.gif";
 import img4 from "../img/scrollbar/noodle.gif";
 import "../sass/scroll.scss";
 
-const images = [img1,
-
-    img2, img3, img4];
+const images = [img1, img2, img3, img4];
 
 function createImageSlider() {
     /*
-      Creates the image slide
-      */
+        Creates the image slide
+        */
+
+    let activeLink = 0;
+    let links = [];
+
     const container = createSetElement("section", {
         class: "sections image-slider",
     });
@@ -20,7 +22,7 @@ function createImageSlider() {
     const wrapper = createSetElement("div", { id: "wrapper" });
 
     const navLinks = createSetElement("div", { class: "navLinks" });
-    const ul = createSetElement("ul");
+    const ul = createSetElement("ul", { class: "ul" });
 
     images.forEach((image, index) => {
         const img = createSetElement("img", {
@@ -35,6 +37,11 @@ function createImageSlider() {
             class: "links",
             "data-number": index + 1,
         });
+        li.itemID = index;
+        li.addEventListener('click', (e) => {
+            changePosition(e.target)
+        })
+        links.push(li);
         ul.appendChild(li);
     });
     const btn1 = createSetElement("button", {
@@ -45,6 +52,41 @@ function createImageSlider() {
         class: "btn right",
         "data-side": "right",
     });
+
+    const btns = [btn1, btn2];
+
+    btns.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            if (e.target.dataset.side === "left") {
+                const id = activeLink <= 0 ? links.length - 1 : activeLink - 1;
+                const link = links[id];
+                changePosition(link);
+            } else if (e.target.dataset.side === "right") {
+                const id = activeLink >= links.length - 1 ? 0 : activeLink + 1;
+                const link = links[id];
+                changePosition(link);
+            }
+        });
+    });
+
+    function changePosition(link) {
+        // console.log(link)
+        links.forEach((linkItem) => {
+            linkItem.classList.remove("active");
+        });
+
+        activeLink = link.itemID
+
+        const no = parseInt(link.dataset.number) - 1
+        const position = no * -100
+
+        const translateValue = `translate3d(${position}vw, 0px, 0px)`
+        wrapper.style.transform = translateValue
+
+        link.classList.add('active')
+
+    }
+
     btn1.innerHTML = "&#10094;";
     btn2.innerHTML = "&#10093;";
 
